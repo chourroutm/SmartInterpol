ef78bce7a7bb60d64bbea1eee35cea94bcf0c7a1
Matthieu Chourrout <83714683+chourroutm@users.noreply.github.com>
2025-04-11 01:01:38 +0100

    Update call_reg_aladin.m

--- SmartInterpol/functions/call_reg_aladin.m
+++ SmartInterpol/functions/call_reg_aladin.m
@@ -1,8 +1,11 @@
 function returnCode=call_reg_aladin(aladin,reference,floating,result,affine_transform,extra_options)
-    cmd = [aladin ' -ref ' reference ' -flo ' floating ' -res ' result  ' -aff ' affine_transform ' ' extra_options];
 
-    returnCode=system([cmd ' >/dev/null']); %execute operating system command and return output
-    % returnCode=system(cmd); %execute operating system command and return output
+    fprintf('   [ skipping affine registration ] \n')
+    % cmd = [aladin ' -ref ' reference ' -flo ' floating ' -res ' result  ' -aff ' affine_transform ' ' extra_options];
+    cmd = [aladin ' -ref ' reference ' -flo ' floating ' -res ' result ' ' extra_options];
+
+    % returnCode=system([cmd ' >/dev/null']); %execute operating system command and return output
+    returnCode=system(cmd); %execute operating system command and return output
 
 end
 
