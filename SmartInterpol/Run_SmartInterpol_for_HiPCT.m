282fc161c3456f2942e3c6fa97f749282c070ca2
Matthieu Chourrout <83714683+chourroutm@users.noreply.github.com>
2025-05-22 17:28:32 +0100

    Update Run_SmartInterpol_for_HiPCT.m

--- SmartInterpol/Run_SmartInterpol_for_HiPCT.m
+++ SmartInterpol/Run_SmartInterpol_for_HiPCT.m
@@ -17,6 +17,6 @@ trained_net_file = '/data/output_from_SmartInterpol/batch_1/trained_net.mat'; % 
 downsampling_factor = 0.25;
 LF_downsampling_factor = 0.25;
 
-SmartInterpol(im_vol,lab_vol , result_path, trained_net_file, 'downsampling_factor', downsampling_factor,'LF_downsampling_factor',LF_downsampling_factor,'LUT_path',LUT_fullPath,'maxepoch_global',3000,'LR_global',0.1,'lambda_DL',2,'minibatch_global',4,'LR_local',0.1,'minibatch_local',4,'firstSliceOption',1,'lastSliceOption',1)
+SmartInterpol(im_vol,lab_vol , result_path, trained_net_file, 'downsampling_factor', downsampling_factor,'LF_downsampling_factor',LF_downsampling_factor,'LUT_path',LUT_fullPath,'maxepoch_global',300,'LR_global',0.1,'lambda_DL',2,'minibatch_global',4,'LR_local',0.1,'minibatch_local',4,'firstSliceOption',1,'lastSliceOption',1)
 
 diary off
